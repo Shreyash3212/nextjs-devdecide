7a5e845066fae2f83db9668bb44d8a9597d75c05
shreyash <shreyashmeshram76@gmail.com>
2026-02-27 01:23:45 +0530

    updated welcome email format.

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -11,7 +11,7 @@ const SubscriberSchema = new mongoose.Schema(
     },
     status: {
       type: String,
-      enum: ["active", "unsubscribed"],
+      enum: ["active", "unsubscribed", "bounced", "complained"],
       default: "active",
     },
   },
